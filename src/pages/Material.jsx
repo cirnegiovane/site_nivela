11a07814f3dd3074cc111b0389d754def1e1d393
cirnegiovane <canalgiovane@hotmail.com>
2025-09-09 21:26:47 -0300

    primeiros passos do mat didatico

--- src/pages/Material.jsx
+++ src/pages/Material.jsx
@@ -1,17 +1,9 @@
+import Listas from "../components/Listas.jsx";
+
 function Material() {
   return (
     <>
-      <div className="bg-green-600 min-h-screen px-5 py-5">
-        <div className="bg-white px-5 py-5 rounded-lg">
-          <h1 className="text-3xl font-bold mb-4 text-center">
-            Página Material Didático em manutenção
-          </h1>
-          <p className="text-lg text-center">
-            ⚠⚠ Esta página está em manutenção. Em breve, mais informações
-            estarão disponíveis aqui. ⚠⚠
-          </p>
-        </div>
-      </div>
+      <Listas />
     </>
   );
 }
